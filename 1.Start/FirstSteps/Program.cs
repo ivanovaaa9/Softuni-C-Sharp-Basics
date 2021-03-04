46a970db34804e74110afaab06af2f253a036943
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-04 18:33:31 +0200

    Add files via upload

---
+++ 1.Start/FirstSteps/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstSteps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello nice day!");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Beep();
+        }
+    }
+}
