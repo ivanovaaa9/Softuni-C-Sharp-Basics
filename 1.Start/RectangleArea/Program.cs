1fe704dd43a4189fee94abe745347d0f3a3bdeda
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-06 18:45:01 +0200

    Add files via upload

---
+++ 1.Start/RectangleArea/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // int a = 2;
+           // int b = 7;
+
+          //  int area = a * b;
+          //  Console.WriteLine(area);
+
+            
+          //  string userN = Console.ReadLine();
+            
+
+          //  string userB = Console.ReadLine();
+         //   Console.WriteLine(userN + userB);
+
+
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+
+            Console.WriteLine("Enter your age:");
+            int age1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age1);
+
+            int area1 = age + age1;
+            Console.WriteLine(area1);
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
