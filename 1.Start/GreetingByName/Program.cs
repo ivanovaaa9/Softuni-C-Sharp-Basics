23210c0f787d2505299c666109f8e9966b1fa6b0
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-07 22:17:48 +0200

    Add files via upload

---
+++ 1.Start/GreetingByName/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01.GreetingsByName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //      int x = 5;
+            //      int y = 10;
+            //      double f = 5.5;
+            //      char symbol = 'a';
+            //      string text = "holla";
+            //      Console.WriteLine();
+            //      float number = 6.6f;
+            //      decimal g = 8.8m;
+
+
+            //      int age = int.Parse(Console.ReadLine());
+            //      Console.WriteLine(age + 15);
+            //
+            //will show my age + 15 , how old s.o will be after 15 years.
+            //int may be also declared as double.
+
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("I am " + age + "years old");
+
+
+
+           
+        }
+    }
+}
