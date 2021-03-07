5baab4b12a23ef5f7bb80e270e9c0c534a5eeb98
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-07 22:16:45 +0200

    Add files via upload

---
+++ 1.Start/TurnPCoff/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace turnPcOff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("shutdown", "/s /t 0");
+        }
+    }
+}
