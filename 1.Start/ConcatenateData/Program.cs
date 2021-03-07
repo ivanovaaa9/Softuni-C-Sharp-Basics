35b7826dbd07b8b9db7861f6b0fa4065fe9083ed
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-07 22:20:48 +0200

    Add files via upload

---
+++ 1.Start/ConcatenateData/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _02.ConcatenateData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Concatenate Data");
+            //odd or even
+            // Console.WriteLine(5%2);
+            // returns the rest of division - 1yes or 0no
+
+            //   string firstN = Console.ReadLine();
+            //   string secondN = Console.ReadLine();
+            //   int age = int.Parse(Console.ReadLine());
+            //   string city = Console.ReadLine();
+            //   //or interpolation , important put $ in front for recognition
+            //   Console.WriteLine($"You are {firstN}, {age} years old");
+            //or
+            //Console.WriteLine("You are {0}{1} , a {2} - year old person, from {3}.",
+            //   firstN, secondN, age, city);
+
+            //  double num = 3.456700;
+            //  Console.WriteLine(Math.Round(num, 10));
+            //or writes without nulls
+            //  Console.WriteLine($"F2 = {num:f10}");
+            //writes with all nulls
+
+
+            Console.Write("First Name: ");
+            string FN = Console.ReadLine();
+
+        
+        }
+
+
+
+    }
+}
