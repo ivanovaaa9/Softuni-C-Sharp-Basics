5baee5c0b3165cf7135c7e6222e1e4310709079a
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-08 21:42:49 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/CelsiustoFahrenheit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03.CelsiustoFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius = double.Parse(Console.ReadLine());
+
+            double fahr = celsius * 1.8 + 32;
+
+            Console.WriteLine($"{fahr:f2}");
+
+
+  
+        }
+    }
+}
