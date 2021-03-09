720124e4596de63ff36de09fff762e8f54782908
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-09 21:40:21 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/TrainingLab/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _05.TrainingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+            // METERS TO SANTIMETERS
+            double metersH = h * 100 / 120;
+            double metersW = (w * 100 - 100) / 70;
+            // function for division with "остатък"
+            //70 size  ширина
+            //120 size  височIна
+            double metersF = Math.Truncate(metersW);
+            double metersG = Math.Truncate(metersH);
+            Console.WriteLine((metersF * metersG) - 3);
+
+
+
+        }
+    }
+}
