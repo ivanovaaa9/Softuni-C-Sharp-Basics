bd18366444298892abb583665f13a3705974c5da
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-09 21:38:58 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/VegetableMarket/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _04.VegetableMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double row1 = double.Parse(Console.ReadLine());
+            double row2 = double.Parse(Console.ReadLine());
+            int row3 = int.Parse(Console.ReadLine());
+            int row4 = int.Parse(Console.ReadLine());
+
+            double priceVeggie = row1 * row3;
+            double priceFruits = row2 * row4;
+            double sum = (priceFruits + priceVeggie) / 1.94;
+
+            Console.WriteLine($"{sum:f2}");
+
+
+
+
+        }
+    }
+}
