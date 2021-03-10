9f2a4520aa1d5dfed85c3cbab1ddf1049b3eb53b
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-10 11:18:35 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/CircleAreaandPerimeter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _08.CircleAreaandPerimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            double area = Math.PI * (r*r);
+            double p = 2 * r * Math.PI;
+            Console.WriteLine($"{area:f2}");
+            Console.WriteLine($"{p:f2}");
+
+
+
+        }
+    }
+}
