a5acd9fcd9bc91a0285d56d2ad19282d13ad1af3
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-10 11:11:39 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/Fishland/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06.Fishland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sk = double.Parse(Console.ReadLine());
+            double caca = double.Parse(Console.ReadLine());
+            double pala = double.Parse(Console.ReadLine());
+            double saf = double.Parse(Console.ReadLine());
+            int midi = int.Parse(Console.ReadLine());
+
+            double pricePala = sk + sk * 0.60;
+            double sumPala = pala * pricePala;
+            double priceSaf = caca + caca * 0.80;
+            double sumSaf = priceSaf * saf;
+            double sumMidi = midi * 7.50;
+
+            double sum = sumPala + sumSaf + sumMidi;
+
+            Console.WriteLine($"{sum:f2}");
+
+           
+        }
+    }
+}
