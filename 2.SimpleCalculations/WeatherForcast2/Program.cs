2bdb5160082876139e18dc16f83ad18386d53a41
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-11 10:44:19 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/WeatherForcast2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _10.WeatherForecast_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+
+            if (num >= 26.00 && num <= 35.00) 
+            {
+                Console.WriteLine("Hot");
+            }
+            else if (num >= 20.1 && num <= 25.9)
+            {
+                Console.WriteLine("Warm");
+            }
+            else if (num >= 15.00 && num <= 20.00)
+            {
+                Console.WriteLine("Mild");
+            }
+            else if (num >= 12.00 && num <= 14.9)
+            {
+                Console.WriteLine("Cool");
+
+            }
+            else if (num >= 5.00 && num <= 11.9)
+            {
+                Console.WriteLine("Cold");
+
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
