2f7f083880163a712b254684cf95d7b9619d92d8
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-11 11:07:43 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/WeatherForecast/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _09.WeatherForecast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            if(word == "sunny")
+            {
+                Console.WriteLine("It's warm outside!");
+            }
+            else
+            {
+                Console.WriteLine("It's cold outside!");
+            }
+        }
+    }
+}
