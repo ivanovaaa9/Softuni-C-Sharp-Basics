c7f382c4b5ff9a5e0e005800b5195dca69554f4c
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-11 11:20:40 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/Book-DailyEarnings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Book.pg97_Repairing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+            double l = double.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int o = int.Parse(Console.ReadLine());
+
+            int sum = n * n;
+            int bench = m * o;
+            int placeCover = sum - bench;
+            double pl = w * l;
+            double nes = placeCover / pl;
+            double time = nes * 0.2;
+
+            Console.WriteLine(nes);
+            Console.WriteLine(time);
+           
+        }
+    }//name change tiles
+}
