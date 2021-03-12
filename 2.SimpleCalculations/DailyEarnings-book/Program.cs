ae130045a41027642f0de4d566c70d9c808d6280
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-12 10:20:37 +0200

    Add files via upload
    Exercises containing the word "book" are from the book for C# - Principles of programming - S.Nakov

---
+++ 2.SimpleCalculations/DailyEarnings-book/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DailyEarnings_book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double money = double.Parse(Console.ReadLine());
+            double DollarLv = double.Parse(Console.ReadLine());
+
+            double SalaryMonth = days * money;
+            double yearIncome = SalaryMonth * 12 + SalaryMonth * 2.5;
+            double tax =  0.25 * yearIncome;
+            double clearIncome = (yearIncome - tax) * DollarLv;
+            double profitDay = clearIncome / 365;
+            Console.WriteLine($"{profitDay:f2}");
+            // or Console.WriteLine("{0:f2}, profitDay);
+            
+        }
+    }
+}
