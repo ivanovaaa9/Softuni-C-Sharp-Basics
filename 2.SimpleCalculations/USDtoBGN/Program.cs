714ab21cb6870d6cfb96382acc307fcd2ead3e84
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-14 14:28:19 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/USDtoBGN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01.UDStoBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dollars = double.Parse(Console.ReadLine());
+            double bgn = dollars * 1.79549;
+
+            Console.WriteLine($"{bgn:F2}");
+        }
+    }
+}
