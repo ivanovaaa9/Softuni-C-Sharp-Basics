c5b3674243e89ededb4909fae1029c9f2de7e97f
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-15 10:16:15 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/RadiansToDegrees/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02.RadiansToDegrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radians = double.Parse(Console.ReadLine());
+
+            double degrees = radians * 180 / Math.PI;
+            Console.WriteLine(Math.Round(degrees)); // закръгляме до цяло число
+        }
+    }
+}
