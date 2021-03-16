02ba0555414fbcc4ac839ba2b74862ff709bc917
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-16 08:24:26 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/SquareArea/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03.SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int sum = a * a;
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
