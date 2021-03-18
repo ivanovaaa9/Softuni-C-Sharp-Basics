4b1e439169d2314d2209f4a044f4630b44e58109
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-18 21:39:40 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/TailoringWorkshop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04.TailoringWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int countTables = int.Parse(Console.ReadLine());
+            double lenghtTable = double.Parse(Console.ReadLine());
+            double wideTables = double.Parse(Console.ReadLine());
+
+            double sumP = countTables * (lenghtTable + 2 * 0.30) * (wideTables + 2 * 0.30);
+            double sumK = countTables * (lenghtTable / 2) * (lenghtTable / 2);
+           
+            double dollar = sumP * 7 + sumK * 9;
+            double lv = dollar * 1.85;
+
+            Console.WriteLine($"{dollar:F2} USD");
+            Console.WriteLine($"{lv:F2} BGN");
+
+
+        }
+    }
+}
