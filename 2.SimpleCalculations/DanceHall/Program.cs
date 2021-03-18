12d97f8a4fcab6a4c87b237d7814e32f46b05b7b
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-18 21:40:56 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/DanceHall/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05.DanceHall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //one person = 7040 sm
+            // free space / by one person space
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double wardropeSide = double.Parse(Console.ReadLine());
+            double areaHall = (lenght * 100) * (width * 100); // in sm
+            double bench = areaHall / 10;
+            double wardrobe = (wardropeSide*100) * (wardropeSide*100); // * 100 to get from meters in santimeters;
+
+            double empthySpace = areaHall - bench - wardrobe;
+            double countPeople = empthySpace / 7040;
+
+            Console.WriteLine(Math.Floor(countPeople));
+
+            
+
+
+
+
+
+            
+
+
+        }
+    }
+}
