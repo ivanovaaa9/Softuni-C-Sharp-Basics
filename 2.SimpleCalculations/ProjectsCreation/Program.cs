a1e964118ab19514716ba7bfda975fcab4aeb9ed
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-19 08:59:22 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/ProjectsCreation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _05.ProjectsCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string architectName = Console.ReadLine();
+            int numProjects = int.Parse(Console.ReadLine());
+            int hours = numProjects * 3;
+
+            Console.WriteLine($"The architect {architectName} will need {hours} hours to complete {numProjects} project/s.");
+        }
+    }
+}
