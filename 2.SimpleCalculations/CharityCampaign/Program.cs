d34f0f40b64b8fafe24fc20466c6eb25b0ac22bf
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-25 10:19:57 +0200

    Add files via upload

---
+++ 2.SimpleCalculations/CharityCampaign/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06.CharityCampaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int people = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int gof = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+
+            double cakesSales = cakes * 45;
+            double gofSales = gof * 5.80;
+            double pancakesSales = pancakes * 3.20;
+
+            double sum = (cakesSales + gofSales + pancakesSales) * people;
+            double Ss = sum * days;
+            double final = Ss - (Ss/8);
+
+            Console.WriteLine($"{final:F2}");
+
+
+
+
+        }
+    }
+}
