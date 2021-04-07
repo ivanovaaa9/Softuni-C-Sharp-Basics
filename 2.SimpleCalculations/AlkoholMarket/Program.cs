2d8ea375f1e5a8046eae706c1b0b015345a36610
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-04-07 22:29:03 +0300

    Add files via upload

---
+++ 2.SimpleCalculations/AlkoholMarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07.AlkoholMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double wiskeyPrice = double.Parse(Console.ReadLine());
+          
+            double beerQuantity = double.Parse(Console.ReadLine());
+            double wineQuantity = double.Parse(Console.ReadLine());
+            double rakiaQuantity = double.Parse(Console.ReadLine());
+            double wiskeyQuantity = double.Parse(Console.ReadLine());
+
+            double rakiaPrice = wiskeyPrice / 2;
+            double sumRakia = rakiaPrice * rakiaQuantity;
+
+            double beerPrice = rakiaPrice - 0.8 * rakiaPrice;
+            double beerSum = beerQuantity * beerPrice;
+
+            double winePrice = rakiaPrice - 0.4 * rakiaPrice;
+            double wineSum = winePrice * wineQuantity;
+            double wiskeySum = wiskeyPrice * wiskeyQuantity;
+
+            double totalSum = sumRakia + beerSum + wineSum + wiskeySum;
+            Console.WriteLine($"{totalSum:F2}");
+
+
+
+
+
+        }
+    }
+}
