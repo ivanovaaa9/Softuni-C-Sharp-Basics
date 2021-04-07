5e20df3a3b32ef57a35ea1eec904535e104ab004
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-04-07 22:27:47 +0300

    Add files via upload

---
+++ 2.SimpleCalculations/PetShop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06.PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogNum = int.Parse(Console.ReadLine());
+            int others = int.Parse(Console.ReadLine());
+           double priceDogsF = 2.50;
+            double othersFood = 4;
+
+            double finalDogs = dogNum * priceDogsF;
+            double finalOthers = others * othersFood;
+            double total = finalDogs + finalOthers;
+            Console.WriteLine($"{total:F2} lv.");
+
+
+        }
+    }
+}
