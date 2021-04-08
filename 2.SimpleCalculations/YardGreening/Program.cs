8a2f66a48bca1eab895ecb3eaca930ed1bce635a
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-04-08 11:31:19 +0300

    Add files via upload

---
+++ 2.SimpleCalculations/YardGreening/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _07.YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Qmeter = double.Parse(Console.ReadLine());
+            
+            double priceForwholeYard = Qmeter * 7.61;
+            double discountPrice = priceForwholeYard * 0.18;
+            double finalPrice = priceForwholeYard - discountPrice;
+            Console.WriteLine($"The final price is: {finalPrice:F2} lv.");
+            Console.WriteLine($" The discount is: {discountPrice:F2} lv.");
+
+
+        }
+    }
+}
