6e4de71e03fb5ea0b00f138fcd67a9c815a843f5
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-04-08 11:32:56 +0300

    Add files via upload

---
+++ 2.SimpleCalculations/UDStoBGN/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UDStoBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dollars = double.Parse(Console.ReadLine());
+            double bgn = dollars * 1.79549;
+
+            Console.WriteLine($"{bgn:F2}");
+        }
+
+
+    }
+}
