b006a414084bfd202c597c35ae8bcf36f59987e5
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-04-09 15:59:58 +0300

    Add files via upload

---
+++ 3.ConditionalStatements/SleepyTommyCat/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _02.SleepyTomCat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int freeDys = int.Parse(Console.ReadLine());
+            int freeDaysinMin = freeDys * 127;
+            int workDays = 365 - freeDys;
+            int MworkDays = workDays * 63;
+            int sumTimePlay = MworkDays + freeDaysinMin;
+
+            int DifferenceInMin = Math.Abs(sumTimePlay - 30000);
+            int diffInHours = DifferenceInMin / 60;
+            double diffMin = DifferenceInMin % 60;
+            if (sumTimePlay > 30000)
+            {
+                Console.WriteLine("Tom will run away");
+                Console.WriteLine($"{diffInHours} hours and {diffMin} minutes more for play");
+            }
+            else
+            {
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine($"{diffInHours} hours and {diffMin} minutes less for play");
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
