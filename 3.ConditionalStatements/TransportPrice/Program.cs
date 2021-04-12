f4e87e6ddad25a9f25f257642f75ef0b94fe3565
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-04-12 08:52:22 +0300

    Add files via upload

---
+++ 3.ConditionalStatements/TransportPrice/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _04.TransportPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numKm = int.Parse(Console.ReadLine());
+            string word = Console.ReadLine();
+         
+            if (word == "day")
+            {
+                if (numKm >= 100)
+                {
+                    double price = numKm * 0.06;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else if (numKm >= 20)
+                {
+                    double price = numKm * 0.09;
+                    Console.WriteLine($"{price:f2}");
+                }
+                 
+                else
+                {
+                    double price = 0.70 + numKm * 0.79;
+                    Console.WriteLine($"{price:f2}");
+                }
+            }
+            else if (word == "night")
+            {
+                if (numKm >= 100)
+                {
+                    double price = numKm * 0.06;
+                    Console.WriteLine($"{price:f2}");
+                }
+                
+                else if (numKm >= 20)
+                {
+                    double price = numKm * 0.09;
+                    Console.WriteLine($"{price:f2}");
+                }
+                else 
+                {
+                    double price = 0.70 + (numKm * 0.90);
+                    Console.WriteLine($"{price:f2}");
+                }
+
+            }
+
+        }
+    }
+}
