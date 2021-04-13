79b8d69af8481d5472b25948389863606d8a4fa4
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-04-13 10:11:13 +0300

    Add files via upload

---
+++ 3.ConditionalStatements/GretaterNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _02.GretaterNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   double result = double.Parse(Console.ReadLine());
+               if (result >= 5.50)
+               {
+                   Console.WriteLine("Excellent!");
+               }
+               */
+            int result = int.Parse(Console.ReadLine());
+            int num = int.Parse(Console.ReadLine());
+
+            if (result > num)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+}
