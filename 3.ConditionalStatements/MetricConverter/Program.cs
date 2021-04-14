ab3ffc5d1240d82f9d98516553fcb01d4065d405
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-04-14 09:57:05 +0300

    Add files via upload

---
+++ 3.ConditionalStatements/MetricConverter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _04.MetricConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            string valueIn = Console.ReadLine();
+            string valueOut = Console.ReadLine();
+
+           if(valueIn == "mm" && valueOut =="m")
+            {
+                double mm = num / 1000;
+                Console.WriteLine($"{mm:f3}");
+            }
+           else if (valueIn == "m" && valueOut == "mm")
+            {
+                double m = num * 1000;
+                Console.WriteLine($"{m:f3}");
+            }
+            else if (valueIn == "m" && valueOut == "cm")
+            {
+                double m = num * 100;
+                Console.WriteLine($"{m:f3}");
+            }
+            else if (valueIn == "cm" && valueOut == "m")
+            {
+                double m = num / 100;
+                Console.WriteLine($"{m:f3}");
+            }
+            else if (valueIn == "cm" && valueOut == "mm")
+            {
+                double m = num * 10;
+                Console.WriteLine($"{m:f3}");
+            }
+            else if (valueIn == "mm" && valueOut == "cm")
+            {
+                double m = num / 10;
+                Console.WriteLine($"{m:f3}");
+            }
+
+        }
+    }
+}
