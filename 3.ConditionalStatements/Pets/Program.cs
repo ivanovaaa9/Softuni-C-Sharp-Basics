be16792451e4a64c9f614f250b5daee0c94f8d93
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-04-15 19:17:26 +0300

    Add files via upload

---
+++ 3.ConditionalStatements/Pets/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06.Pets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int foodKg = int.Parse(Console.ReadLine());
+            double dayFDog = double.Parse(Console.ReadLine());
+            double dayFCAt = double.Parse(Console.ReadLine());
+            double dayFturt = double.Parse(Console.ReadLine());
+
+            double convert = dayFturt / 1000;
+
+            double dog = days * dayFDog;
+            double cat = days * dayFCAt;
+            double turtle = days * convert;
+
+            double sumF = dog + cat + turtle;
+            if(sumF <= foodKg)
+            {
+                double sum = foodKg - sumF;
+                Console.WriteLine($"{Math.Floor(sum)} kilos of food left.");
+            }
+            else
+            {
+                double sum = sumF - foodKg;
+                Console.WriteLine($"{Math.Ceiling(sum)} more kilos of food are needed.");
+            }
+
+
+
+        }
+    }
+}
