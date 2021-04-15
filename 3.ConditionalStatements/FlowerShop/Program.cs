ab3a99d25a00cecd05d4fd2c327b5a0f145f34bc
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-04-15 19:18:59 +0300

    Add files via upload

---
+++ 3.ConditionalStatements/FlowerShop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07.FlowerShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mangolii = int.Parse(Console.ReadLine());
+            int zu = int.Parse(Console.ReadLine());
+            int roses = int.Parse(Console.ReadLine());
+            int cactus = int.Parse(Console.ReadLine());
+            double price = double.Parse(Console.ReadLine());
+
+            double sum = mangolii * 3.25 + zu * 4 + roses * 3.5 + cactus * 8;
+            double win = sum - sum * 0.05;
+            if( price > win)
+            {
+                double diff = price - win;
+                Console.WriteLine($"She will have to borrow {Math.Ceiling(diff)} leva." );
+            }
+            else
+            {
+                double diff = win - price;
+                Console.WriteLine($"She is left with {Math.Floor(diff)} leva.");
+            }
+        }
+    }
+}
