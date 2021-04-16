25401b1be6d4c3dc6f6a928338ad151ed060aa8d
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-04-16 18:37:43 +0300

    Add files via upload

---
+++ 3.ConditionalStatements/WorldSwimmingRecord/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07.WorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double timePerMeter = double.Parse(Console.ReadLine());
+            //math.floor - delays in every 15 m * пътите в който се забавя
+            double swimTime = distance * timePerMeter;
+            double delayTime = Math.Floor(distance / 15 ) * 12.5;
+            double totalTime = swimTime + delayTime;
+            // better record?
+            if (totalTime < record)
+            {
+                Console.WriteLine($" Yes, he succeeded! The new world record is {totalTime:f2} seconds.");
+            }
+            else
+            {
+                double needSek = totalTime - record;
+                Console.WriteLine($"No, he failed! He was {needSek:f2} seconds slower.");
+            }
+
+
+
+        }
+    }
+}
