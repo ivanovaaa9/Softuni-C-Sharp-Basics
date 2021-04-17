55779ae250618ac3714f44877db1a9e9fbc4d5dd
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-04-17 08:50:50 +0300

    Add files via upload

---
+++ 3.ConditionalStatements/Scholarship/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _08.Scholarship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             double income = double.Parse(Console.ReadLine());
+              double grades = double.Parse(Console.ReadLine());
+              double minwage = double.Parse(Console.ReadLine());
+            double soc = minwage * 35 / 100;
+            double Schgr = grades * 25;            
+            if ( grades >= 5.5)
+            {
+                if (Schgr >= soc || income > minwage )        //  || or operator, check first after that second , but if first is not true it will not check the second  
+                    Console.WriteLine("You get a scholarship for excellent results {0} BGN", Math.Floor(Schgr));                
+                else                
+                    Console.WriteLine("You get a Social scholarship {0} BGN", Math.Floor(soc));                
+            }            
+             else if (income < minwage && grades > 4.5) // && highest priority, means AND
+              {
+                  
+                  Console.WriteLine("You get a Social scholarship {0} BGN", Math.Floor(soc));
+              }
+        
+              else
+              
+                Console.WriteLine("You cannot get a scholarship!");
+               
+            
+        }
+    }
+}
