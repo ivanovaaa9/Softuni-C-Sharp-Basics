567c21118db40df1f7e5d3b81deb340207da77ff
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-01 21:19:10 +0300

    Add files via upload

---
+++ 3.ConditionalStatements/scoolarship 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace scoolarship_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double income = double.Parse(Console.ReadLine());
+            double results = double.Parse(Console.ReadLine());
+            double minSalary = double.Parse(Console.ReadLine());
+            double scholarshipS = minSalary * 35 / 100;//социална стипендия
+            double scholarshipE = results * 25;//за успех стидендия
+            if (results >= 5.5)
+            {
+                if (scholarshipE >= scholarshipS || income > minSalary)
+                    Console.WriteLine("You get a scholarship for excellent results {0} BGN", Math.Floor(scholarshipE));
+                else Console.WriteLine("You get a Social scholarship {0} BGN", Math.Floor(scholarshipS));
+            }
+            else if (income < minSalary && results > 4.5)
+            {
+                Console.WriteLine("You get a Social scholarship {0} BGN", Math.Floor(scholarshipS));
+            }
+
+            else Console.WriteLine("You cannot get a scholarship!");
+        }
+    }
+}
