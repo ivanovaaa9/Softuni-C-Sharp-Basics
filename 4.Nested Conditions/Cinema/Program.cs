27a4fc0254a0524918a1d00651add09c58bb3d2e
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-06 20:30:41 +0300

    Add files via upload

---
+++ 4.Nested Conditions/Cinema/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02.Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeProjections = Console.ReadLine();
+            int numRows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+
+            double income = 0.0;
+            if(typeProjections == "Premiere")
+            {
+                income = numRows * columns * 12;
+                Console.WriteLine("{0:f2} leva", income);
+            }else if(typeProjections == "Normal")
+            {
+                income = numRows * columns * 7.5;
+                Console.WriteLine("{0:f2} leva", income);
+            }else if(typeProjections == "Discount")
+            {
+                income = numRows * columns * 5;
+                Console.WriteLine("{0:f2} leva", income);
+            }
+            //can be only cw+tap tap at the end to avoid repetition
+
+        }
+    }
+}
