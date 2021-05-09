6b0dd3e9fe3a81864139f6beb95c4123636324dc
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-09 16:18:34 +0300

    Add files via upload

---
+++ 4.Nested Conditions/Journey/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _06._Journey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string destination = "";
+            string room = "";
+            switch (season)
+            {
+                case "summer":
+                    
+                    if(budget <= 100)
+                    {
+                        room = "Camp";
+                        destination = "Bulgaria";
+                        budget = budget * 0.30;
+
+                    }else if(budget <= 1000)
+                    {
+                        room = "Camp";
+                        destination = "Balkans";
+                        budget = budget * 0.40;
+                    }else if(budget > 1000)
+                    {
+                        room = "Hotel";
+                        destination = "Europe";
+                        budget = budget * 0.90;
+                    }
+                    break;
+                case "winter":
+                    room = "Hotel";
+                    if (budget <= 100)
+                    {
+                        room = "Hotel";
+                        destination = "Bulgaria";
+                        budget = budget * 0.70;
+
+                    }
+                    else if (budget <= 1000)
+                    {
+                        room = "Hotel";
+                        destination = "Balkans";
+                        budget = budget * 0.80;
+                    }
+                    else if (budget > 1000)
+                    {
+                        room = "Hotel";
+                        destination = "Europe";
+                        budget = budget * 0.90;
+                    }
+                    break;
+            }
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{room} - {budget:f2}");
+                
+
+        }
+    }
+}
