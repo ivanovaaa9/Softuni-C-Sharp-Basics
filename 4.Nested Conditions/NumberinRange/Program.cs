01303f6c74f83c5b133ba2a2e91818697f8bd898
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-09 16:19:20 +0300

    Add files via upload

---
+++ 4.Nested Conditions/NumberinRange/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _06.NumberinRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            /* switch (num)
+             {
+                 case 0:
+                     Console.WriteLine("No");
+                         break;
+                 default:
+                     Console.WriteLine("Yes");
+                     break;
+             }*/
+            //71/100.
+            
+            if (num >=-100 && num <= 100 && num !=0)
+            {
+                Console.WriteLine("Yes");
+            }
+            
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+            
+
+
+        }
+    }
+}
+
+
+
