e3d9fe12529229f4f8ed973a39ed7c120f608f0c
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-09 16:17:39 +0300

    Add files via upload

---
+++ 4.Nested Conditions/Vacation/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _05.Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            string place = "";
+            string location = "";
+            double price = 0.0;
+            if (budget <= 1000)
+            {
+                place = "Camp";
+                switch (season)
+                {
+                    case "Summer":
+                        location = "Alaska";
+                        price = budget * 0.65;
+                        break;
+                    case "Winter":
+                        location = "Morocco";
+                        price = budget * 0.45;
+                        break;
+                }
+            }
+            if (budget > 1000 && budget <= 3000)
+            {
+                place = "Hut";
+                switch (season)
+                {
+                    case "Summer":
+                        location = "Alaska";
+                        price = budget * 0.80;
+                        break;
+                    case "Winter":
+                        location = "Morocco";
+                        price = budget * 0.60;
+                        break;
+                }
+            }
+            if (budget > 3000)
+            {
+                place = "Hotel";
+                switch (season)
+                {
+                    case "Summer":
+                        location = "Alaska";
+                        price = budget * 0.90;
+                        break;
+                    case "Winter":
+                        location = "Morocco";
+                        price = budget * 0.90;
+                        break;
+                }
+            }
+            
+            Console.WriteLine($"{location} - {place} - {price:f2}");
+        }
+    }
+}
