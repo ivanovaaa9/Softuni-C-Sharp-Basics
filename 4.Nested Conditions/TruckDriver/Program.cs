276ac1802f0a2fe548dad0099e245151aa247336
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-10 11:55:35 +0300

    Add files via upload

---
+++ 4.Nested Conditions/TruckDriver/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _06.TruckDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string seasion = Console.ReadLine();
+            double km = double.Parse(Console.ReadLine());
+            double price = 0.0;
+
+            switch (seasion)
+            {
+                case "Spring":                 
+                case "Autumn":
+                    if(km <= 5000)
+                    {
+                        price = 0.75 * km ;
+                    }else if(km > 5000 && km <= 10000)
+                    {
+                        price = 0.95 * km;
+                    }else if (km > 10000 && km <= 20000)
+                    {
+                        price = 1.45 * km;
+                    }
+                    break;
+                case "Summer":
+                    if (km <= 5000)
+                    {
+                        price = 0.90 * km;
+                    }
+                    else if (km > 5000 && km <= 10000)
+                    {
+                        price = 1.10 * km;
+                    }
+                    else if (km > 10000 && km <= 20000)
+                    {
+                        price = 1.45 * km;
+                    }
+                    break;
+                case "Winter":
+                    if (km <= 5000)
+                    {
+                        price = 1.05 * km;
+                    }
+                    else if (km > 5000 && km <= 10000)
+                    {
+                        price = 1.25 * km;
+                    }
+                    else if (km > 10000 && km <= 20000)
+                    {
+                        price = 1.45 * km;
+                    }
+                    break;
+            }
+            double neew = price * 4;
+            double lasts = neew - (0.10 * neew);
+            Console.WriteLine($"{lasts:f2}");
+            
+        }
+    }
+}
