427ecb8ac6172889dd754a61e0a1fa00ab67bc08
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-11 10:31:08 +0300

    Add files via upload

---
+++ 4.Nested Conditions/Multiplyby2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _09.Multiplyby2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   double i;
+              // var random = new Random();
+
+               for (i = 1; ;i++) {
+                   if (i > 0)
+                   {
+                       Console.WriteLine("Result: " + i * 2);
+                   }
+                   else
+                   {
+                       Console.WriteLine("Negative number!");
+                   }*/
+            double num = 0;
+            double sum = 0;
+                while(num >= 0)
+            {
+                num = double.Parse(Console.ReadLine());
+                num++;
+                if (num > 0)
+                {
+                    sum = num * 2 - 2;
+                    Console.WriteLine($"Result: {sum:f2}");
+                }
+            }
+
+            if (num < 0)
+            {
+                
+                Console.WriteLine("Negative number!");
+                
+            }
+
+
+
+        } }
+    }
+
