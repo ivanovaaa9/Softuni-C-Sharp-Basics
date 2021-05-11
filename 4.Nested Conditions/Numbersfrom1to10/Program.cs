c0d3cd8e7dee02652e3f37e2c9012efcfb2606d1
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-11 10:29:24 +0300

    Add files via upload

---
+++ 4.Nested Conditions/Numbersfrom1to10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _08.Numbersfrom1to10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int i;
+
+            for (i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
