e915b008e6d6191dcf10ae8672cab1ba7450258c
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-14 13:50:20 +0300

    Add files via upload

---
+++ 4.Nested Conditions/Volleyball/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _10.Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int numCeleb = int.Parse(Console.ReadLine());
+            int gamesSundayHomeT = int.Parse(Console.ReadLine());
+
+            double gameSfWeekend = ((48-gamesSundayHomeT) * 0.75);
+
+            double playSfCelebr = numCeleb * 0.66666667;
+            double totalGmes = gameSfWeekend + gamesSundayHomeT + playSfCelebr;
+
+            if (word == "leap")
+            {
+
+                double additional = totalGmes * 0.15;
+                double total = totalGmes + additional;
+                Console.WriteLine($"{Math.Floor(total)}");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Floor(totalGmes)}");
+
+            }
+        }
+    }
+}
