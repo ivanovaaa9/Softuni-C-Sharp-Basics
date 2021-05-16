824c75697e4e4d420c99a6830668601d2ac21710
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-16 10:16:54 +0300

    Add files via upload

---
+++ 5.For-loop/Hospital/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02.Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int period = int.Parse(Console.ReadLine());
+            int medics = 7;
+            int trated = 0;
+            int notTreated = 0;
+            for(int i = 1; i <= period; i++)
+            {
+                int patients = int.Parse(Console.ReadLine());
+                if (i % 3 == 0 && notTreated > trated)
+                {
+                    medics++;
+                }
+                if (patients <= medics)
+                {
+                    trated += patients;
+                }
+                else
+                {
+                    notTreated += patients - medics;
+                    trated += medics;
+                   
+                }
+            }
+            Console.WriteLine("Treated patients: " + trated + ".") ;
+            Console.WriteLine("Untreated patients: " + notTreated + ".");
+        }
+    }
+}
