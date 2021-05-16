9891ef7bb2d99f8be4ae607799333fe3a263e8ac
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-16 10:21:45 +0300

    Add files via upload

---
+++ 5.For-loop/Numbers1-n,th3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03.Numbers1_n_th3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int num = int.MinValue;
+            Console.WriteLine(num-2);*/
+            int num = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= num; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+
+            }
+        }
+    }
+
