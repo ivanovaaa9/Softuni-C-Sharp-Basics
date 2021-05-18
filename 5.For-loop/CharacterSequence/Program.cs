744abe1e83393e2f9bb4a16bd6dd0212a3e7bbd9
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-18 22:47:49 +0300

    Add files via upload

---
+++ 5.For-loop/CharacterSequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05.CharacterSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // string text = "SoftUni";
+            // Console.WriteLine(text.Length);//7 
+            //Console.WriteLine(text[2]);//f - because we start from 0;
+            /*  Console.WriteLine(text[0]);
+              Console.WriteLine(text[1]);
+              Console.WriteLine(text[2]);
+              Console.WriteLine(text[3]);
+              Console.WriteLine(text[4]);
+              Console.WriteLine(text[5]);
+              ...... - use cycles*/
+
+           /* for (int i = 0; i <= 6; i++)
+            {
+                Console.WriteLine(text[i]);
+            }*/
+            //or
+           string text = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.WriteLine(text[i]);
+            }
+
+        }
+    }
+}
