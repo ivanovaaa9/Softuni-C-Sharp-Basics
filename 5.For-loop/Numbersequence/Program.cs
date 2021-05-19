1b97f75ea2596874a3f0e645738c572f7c13391f
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-19 12:52:54 +0300

    Add files via upload

---
+++ 5.For-loop/Numbersequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _08.Numbersequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int minNum = int.MaxValue;
+            int maxNum = int.MinValue;
+
+             for (int i = 0; i < num; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if(currentNum > maxNum)
+                {
+                    maxNum = currentNum;
+                }
+                if (currentNum<minNum)
+                {
+                    minNum = currentNum;                }
+            }
+            Console.WriteLine($"Max number: {maxNum}");
+            Console.WriteLine($"Min number: {minNum}");
+
+
+
+
+        }
+    }
+}
