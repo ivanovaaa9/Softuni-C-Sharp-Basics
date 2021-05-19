5c36caa9dcb9155890b37c8ebf058e43e8cd2cc0
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-19 12:51:17 +0300

    Add files via upload

---
+++ 5.For-loop/SumNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;// now no sum num
+            for (int i = 1; i <= n; i++)
+                //read n numbers, if sum was in for cycle it will be занулена всеки път цикълът се завърта., а ние искаме да я запазим
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+            }
+            //sum them
+            Console.WriteLine(sum);
+
+
+
+        }
+    }
+}
