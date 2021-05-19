6f060635169f9d2ba6e36998cc6bda34d1705ce1
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-19 12:49:31 +0300

    Add files via upload

---
+++ 5.For-loop/VowelsSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06.VowelsSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int value = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i].Equals('a')){
+                    value += 1;
+                }
+                if (text[i].Equals('e'))
+                {
+                    value += 2;
+                }
+                if (text[i].Equals('i'))
+                {
+                    value += 3;
+                }
+                if (text[i].Equals('o'))
+                {
+                    value += 4;
+                }
+                if (text[i].Equals('u'))
+                {
+                    value += 5;
+                }
+            }
+            
+            Console.WriteLine(value);
+            }
+
+        }
+    }
+
