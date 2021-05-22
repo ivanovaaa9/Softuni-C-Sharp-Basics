e5a5d4fdd48aed5eaf402e4602970d1ffa13149a
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-22 14:14:53 +0300

    Add files via upload

---
+++ 5.For-loop/Clock/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _09._Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            { 
+
+            } while (true);
+        }
+    }
+}
