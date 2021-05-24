074b4e369b192c205b3cf45780b70c392b427948
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-24 23:43:00 +0300

    Add files via upload

---
+++ 5.For-loop/num1toN/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace num1toN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+           /* 1 to n
+            * for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(i);
+            }*/
+            for (int i = num; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
