83bffc9f1308a678ed8d4fea79793c2db601091a
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-24 23:42:08 +0300

    Add files via upload

---
+++ 5.For-loop/Num1to100/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace Num1to100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <=100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+             /*   for(int counter = 0; counter < 5; counter += 1)
+            {
+                Console.WriteLine("HEY");
+                Console.WriteLine("Smileee");
+            }*/
+        }
+    }
+}
