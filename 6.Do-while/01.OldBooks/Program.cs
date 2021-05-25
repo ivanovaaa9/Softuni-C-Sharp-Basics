749e0b1310d6501b5e61e81e8b031ec0c77dcf10
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-25 12:18:59 +0300

    Add files via upload

---
+++ 6.Do-while/01.OldBooks/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01.OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countCheckedBookd = 0;
+            string searchedBook = Console.ReadLine();
+            int countToatalBooks = int.Parse(Console.ReadLine());
+            // see whether we have left books for checking
+            while(countToatalBooks > 0)
+            {
+                string title = Console.ReadLine();
+                if (title == searchedBook)
+                {
+                    Console.WriteLine($"You checked {countCheckedBookd} books and found it.");
+                    break;
+                }
+                else
+                {
+                    countCheckedBookd++;
+                }
+                countToatalBooks--;
+                
+            }
+            if (countToatalBooks <= 0)
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {countCheckedBookd} books.");
+            }
+        }
+    }
+}
