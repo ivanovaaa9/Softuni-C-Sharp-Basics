8e2947e13c5b74324707b8ca3f398062a029eba8
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-25 12:18:31 +0300

    Add files via upload

---
+++ 6.Do-while/01. Read Text/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Read_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string input = Console.ReadLine();
+            while (input != "Stop")
+            {
+               /* if(input == "Paris")//when needed 2 conditions
+                {
+                    break;
+                }*/
+                input = Console.ReadLine();
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
