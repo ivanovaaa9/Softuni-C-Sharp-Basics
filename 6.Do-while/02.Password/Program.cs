ff22e498b9d244734cc256456ba696b5ae6103c8
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-26 22:59:03 +0300

    Add files via upload

---
+++ 6.Do-while/02.Password/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _02._Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+            string input = Console.ReadLine();
+            int counter = 1;
+            while(input!= password)
+            {
+                if(counter == 5)
+                {
+                    Console.WriteLine("Incorrect pass!!!!");
+                    break;
+                }
+                input = Console.ReadLine();
+                counter++;
+            }
+            if (input == password) {
+                Console.WriteLine("Welcome " + username + "!");
+            }
+
+        }
+    }
+}
