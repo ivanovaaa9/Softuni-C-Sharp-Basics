a5121005f6dc950723b5bf82e15dcab1c75d8356
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-27 23:31:43 +0300

    Add files via upload

---
+++ 6.Do-while/03.SumNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double sum = 0;
+            while (input != "Stop")
+            {
+                sum += double.Parse(input);
+                input = Console.ReadLine(); // change the beginning value
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
