b1bd8398f118525b5c2844d73f2a306781e05d15
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-31 22:18:34 +0300

    Add files via upload

---
+++ 05.Account Balance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _05._Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int transaction = int.Parse(Console.ReadLine());
+            double bankA = 0;
+            for(int i = 0; i < transaction; i++)
+            {
+                double money = double.Parse(Console.ReadLine());
+                if (money < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                        break;
+                }
+                bankA += money;
+                Console.WriteLine($"Increase: {money:f2}");
+            }
+            Console.WriteLine($"Total: {bankA:f2}");
+        }
+    }
+}
