84750c00c82d92377713d491fd0b98471d00f56f
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-05-31 22:19:19 +0300

    Add files via upload

---
+++ 6.Do-while/05.Average Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05._Average_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = 0;
+            double sum = 0;
+            int num1 = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= num1; i++)
+            {
+                int num2 = int.Parse(Console.ReadLine());
+                num += num2;
+                sum = num / num1;
+
+            }
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
