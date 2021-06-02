d0a54cbcf182170e5e99e4965f52027981192682
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-06-02 22:49:55 +0300

    Add files via upload

---
+++ 6.Do-while/06.Cake/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            double size = width * lenght;
+            int pieceTaken = 0;
+           
+            string com = Console.ReadLine().ToUpper();
+
+            while (com != "STOP")
+            {
+
+                pieceTaken = int.Parse(com);
+                size -= pieceTaken;
+
+                if (size < 0)
+                {
+
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(size)} pieces more.");
+                    break;
+                }
+                com = Console.ReadLine();
+            }
+                if (com == "STOP")
+                {
+                   
+                    Console.WriteLine($"{size} pieces are left.");
+                }
+            
+
+
+        }
+    }
+}
