9ed76c011e8be9752543c1c32833229dde1f96ec
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-06-02 22:50:48 +0300

    Add files via upload

---
+++ 6.Do-while/06.MaxNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06._Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int maxN = int.MinValue;
+            int count = 0;
+            while (count != n)
+            {
+                int newN = int.Parse(Console.ReadLine());
+                if (newN > maxN)
+                {
+                    maxN = newN;
+
+                }
+                count++;
+
+            }
+            Console.WriteLine(maxN);
+
+        }
+    }
+}
