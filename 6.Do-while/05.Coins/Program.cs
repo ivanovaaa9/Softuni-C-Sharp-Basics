9446ec040ce92640445e09718e90684eb6324616
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-06-02 22:49:08 +0300

    Add files via upload

---
+++ 6.Do-while/05.Coins/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _05._Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            double change = double.Parse(Console.ReadLine());
+            double changeinStotinki =Math.Floor(change * 100);
+            while (changeinStotinki != 0)
+            {
+                if(changeinStotinki > 200)
+                {
+                    changeinStotinki -= 200;
+                    count++;
+                }
+                else if (changeinStotinki >= 100)
+                {
+                    changeinStotinki -= 100;
+                    count++;
+                }
+                else if (changeinStotinki >= 50)
+                {
+                    changeinStotinki -= 50;
+                    count++;
+                }
+                else if (changeinStotinki >= 20)
+                {
+                    changeinStotinki -= 20;
+                    count++;
+                }
+                else if (changeinStotinki >= 10)
+                {
+                    changeinStotinki -= 10;
+                    count++;
+                }
+                else if (changeinStotinki >= 5)
+                {
+                    changeinStotinki -= 5;
+                    count++;
+                }
+                else if (changeinStotinki >= 2)
+                {
+                    changeinStotinki -= 2;
+                    count++;
+                }
+                else if (changeinStotinki >= 1)
+                {
+                    changeinStotinki -= 1;
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
