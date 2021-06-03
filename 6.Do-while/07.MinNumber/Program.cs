41794de4ebed80f806cf777f219df6bf343153a0
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-06-03 13:00:01 +0300

    Add files via upload

---
+++ 6.Do-while/07.MinNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07._Min_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int min = int.MaxValue;
+            int count = 0;
+            while(count != n)
+            {
+                int newN = int.Parse(Console.ReadLine());
+                if(newN < min)
+                {
+                    min = newN;
+                }
+                count++;
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
