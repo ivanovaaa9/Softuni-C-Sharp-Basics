83eba56718eeabc709520476b542fdcb25cdd88a
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-06-03 13:01:19 +0300

    Add files via upload

---
+++ 6.Do-while/08.Graduation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _08._Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();           
+            double sum = 0;
+            double grades = 1;
+            while( grades <= 12)
+            {
+               double grade = double.Parse(Console.ReadLine());
+                if (grade>= 4)
+                {
+                    sum += grade;
+                    grades++;
+                    
+                }
+
+            }
+            if (grades == 13)
+            {
+                sum = sum / 12;
+                Console.WriteLine($"{name} graduated. Average grade: {sum:f2}");
+
+            }
+
+        }
+    }
+}
