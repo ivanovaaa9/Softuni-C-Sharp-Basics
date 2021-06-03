b9f34322445721f064098f9436a80f60318441b0
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-06-03 13:02:10 +0300

    Add files via upload

---
+++ 6.Do-while/09.Graduation-pt.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _09._Graduation_pt._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double sum = 0;
+            double grades = 1;
+            int fail = 0;
+            while (grades <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                if (grade >= 4)
+                {
+                    sum += grade;
+                    grades++;
+
+                }if(grade < 4)
+                {
+                    fail++;
+                    if (fail > 1) 
+                    { break;
+                    }
+
+                    }
+
+            }
+            if (grades == 13)
+            {
+                sum = sum / 12;
+                Console.WriteLine($"{name} graduated. Average grade: {sum:f2}");
+
+            }
+            else
+            {
+                Console.WriteLine($"{name} has been excluded at {grades} grade");
+
+            }
+
+        }
+    }
+}
