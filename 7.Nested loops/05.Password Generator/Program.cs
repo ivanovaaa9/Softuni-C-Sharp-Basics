a36573e1adba792d86362003e061d81fe89d3389
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-06-15 18:01:14 +0300

    Add files via upload

---
+++ 7.Nested loops/05.Password Generator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05._Password_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+
+            for (int sym1 = 1; sym1 <= n; sym1++)
+            {
+                for (int sym2 = 1; sym2 <= n; sym2++)
+                {
+                    for (char sym3 = 'a'; sym3 < 'a'+l; sym3++)
+                    {
+                        for (char sym4 = 'a'; sym4 <'a'+l;sym4++)
+                        {
+                            for (int sym5 = 1; sym5 <=n ; sym5++)
+                            {
+                                if (sym5 > sym1 && sym5 > sym2)
+                                {
+                                    Console.Write($"{sym1}{sym2}{sym3}{sym4}{sym5} ");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
