bcf4e48373b8407aca5a6cf72a2253aea7e5056f
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-06-15 18:01:56 +0300

    Add files via upload

---
+++ 7.Nested loops/05.Travelling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            while (destination!="End")
+            {
+                double budget = double.Parse(Console.ReadLine());
+                double savedMoney = double.Parse(Console.ReadLine());
+                double totalMoneySaved = savedMoney;
+                while (totalMoneySaved<budget)
+                {
+                    savedMoney = double.Parse(Console.ReadLine());
+                    totalMoneySaved += savedMoney;
+                }
+                Console.WriteLine($"Going to {destination}!");
+
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
