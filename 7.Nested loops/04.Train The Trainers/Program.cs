3620c6771699be0ebe5291c52a7a387875df60bb
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-06-15 18:00:10 +0300

    Add files via upload

---
+++ 7.Nested loops/04.Train The Trainers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04._Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countJudges = int.Parse(Console.ReadLine());
+            string text = Console.ReadLine();
+            double totalSum = 0;
+            int countGrades = 0;
+            while(text != "Finish")
+            {
+                double sum = 0;
+                for(int i = 1; i <=countJudges; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    sum += grade;
+
+                    totalSum += grade;
+                    countGrades++;
+                    
+                }
+                double average = sum / countJudges;
+                Console.WriteLine($"{text} - {average:f2}.");
+                text = Console.ReadLine();
+            }
+            double nue = totalSum / countGrades;
+            Console.WriteLine($"Student's final assessment is {nue:f2}.");
+        }
+    }
+}
