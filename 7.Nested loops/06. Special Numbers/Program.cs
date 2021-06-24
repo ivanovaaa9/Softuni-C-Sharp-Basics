c74369a9b363400f627f286792469f7023b23648
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-06-24 10:51:13 +0300

    Add files via upload

---
+++ 7.Nested loops/06. Special Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06._Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1111; i <= 9999; i++)
+            {
+                int thousand = i / 1000;
+                int hund = (i / 100) % 10;
+                int tens = (i / 10) % 10;
+                int units = i % 10;
+                //if it is special
+                bool chek1 =thousand!=0 && n % thousand == 0;
+                bool check2 = hund != 0 && n % hund == 0;
+                bool check3 = tens != 0 && n % tens == 0;
+                bool check4 = units != 0 && n % units == 0;
+
+                if (chek1 && check2 && check3 && check4)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
