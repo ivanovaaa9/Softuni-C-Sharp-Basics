e0aff07ea0d75e1e854d5a6cfb4ddb461a367afd
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-06-24 10:50:28 +0300

    Add files via upload

---
+++ 7.Nested loops/06. Building/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace _06._Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int floorsNum = int.Parse(Console.ReadLine());
+            int Rooms = int.Parse(Console.ReadLine());
+
+            for (int i = floorsNum; i >=1; i--){
+                for(int j = 0; j < Rooms; j++)
+                {
+                    if(floorsNum == i)
+                    {
+                        Console.Write($"L{i}{j} ");
+                    }
+                    else if (i % 2 == 0)
+                    {
+                        Console.Write($"O{i}{j} ");
+                    }
+                     else {
+                        Console.Write($"A{i}{j} ");
+                    }
+                   
+
+                }
+                Console.WriteLine("");
+
+            }
+        }
+    }
+}
