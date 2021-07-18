94c3e3cbf7ebafbf2c501645fe40034d07f657a5
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-07-18 21:42:05 +0300

    Add files via upload

---
+++ Exam Preparation/01.Saving/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01.Saving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double income = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double sumNeeded = double.Parse(Console.ReadLine());
+
+            double saved = income - ((income * 0.3) + sumNeeded);
+            double savedMoney = saved * months;
+            double percentage = (saved / income) * 100;
+
+            Console.WriteLine($"She can save {percentage:f2}%");
+            Console.WriteLine($"{savedMoney:f2}");
+
+
+
+        }
+    }
+}
