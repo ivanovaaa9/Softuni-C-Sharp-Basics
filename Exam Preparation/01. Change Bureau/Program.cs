676dd69855e1cb1f5c1bdeb244258983cdc8b808
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-07-18 21:37:46 +0300

    Add files via upload

---
+++ Exam Preparation/01. Change Bureau/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01._Change_Bureau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bitcoin = int.Parse(Console.ReadLine());
+            double chineseJuan = double.Parse(Console.ReadLine());
+            double comission = double.Parse(Console.ReadLine());
+
+            int bitcoinValueBGN = 1168;
+            double chineseJuanValueUSD = 0.15;           
+            double USDBGN = 1.76;
+            double EUROBGN = 1.95;
+            double bitcoinV = bitcoin * bitcoinValueBGN;
+            double Juan = chineseJuan * chineseJuanValueUSD;
+            double JuanBgn = Juan * USDBGN;
+            double sum = bitcoinV + JuanBgn;
+            double sumEuro = sum / EUROBGN;
+            double afterComission =sumEuro- sumEuro * (comission/100);
+            Console.WriteLine($"{afterComission:f2}");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
