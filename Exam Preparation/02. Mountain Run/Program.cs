0945909590bb3039b06d2f3586512296b77faa67
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-07-18 21:42:36 +0300

    Add files via upload

---
+++ Exam Preparation/02. Mountain Run/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02._Mountain_Run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double meters = double.Parse(Console.ReadLine());
+            double timeSekFor1M = double.Parse(Console.ReadLine());
+
+            double clim = meters * timeSekFor1M;
+            double sekAdded = (Math.Floor(meters / 50)) * 30;
+            double total = clim + sekAdded;
+            if (total >= record)
+            {
+                Console.WriteLine($"No! He was {total-record:f2} seconds slower.");
+            }
+            else if (total < record)
+            {
+                Console.WriteLine($" Yes! The new record is {total:f2} seconds.");
+            }
+            
+
+        }
+    }
+}
