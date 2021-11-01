7e3a23110ee401658e0c31b9f6420ed2589d1d90
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-11-01 18:55:35 +0200

    Add files via upload

---
+++ Exam Preparation/05.Problem Solved/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int students = int.Parse(Console.ReadLine());
+            int tasks = int.Parse(Console.ReadLine());
+            int energy = int.Parse(Console.ReadLine());
+            int sumTasks = 0;
+            int sum = 0;
+            int questions=0;
+            
+
+            while (true)
+            {
+                for (int i = 1; i <= tasks; i++)
+                {
+                    energy += tasks * 2;
+                    students -= tasks;
+
+                     questions = students * 2;
+                    energy -= questions * 3;
+                    sumTasks++;
+                    if (students < 10)
+                    {
+                        sum = sumTasks+questions;
+                        Console.WriteLine("The students are too few!");
+                        Console.WriteLine($"Problem solved: {sum}");
+                        return;
+                       }
+                    else if (energy <= 0)
+
+                    {
+                        Console.WriteLine("Trainer is too tired!");
+                        Console.WriteLine($"Questions asked: {questions}");
+                        return;
+                    }
+                    else if (students % 10 == 0)
+                    {
+                        students++;
+                    }
+
+                }
+                
+            }
+         
+
+
+        }
+    }
+}
