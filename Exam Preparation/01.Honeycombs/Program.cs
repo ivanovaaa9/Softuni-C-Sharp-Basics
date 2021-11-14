48b7b4d7f2774684416f232708eca9266793a1fb
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-11-14 12:58:59 +0200

    Add files via upload

---
+++ Exam Preparation/01.Honeycombs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.Honeycombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bees = int.Parse(Console.ReadLine());
+            int flowers = int.Parse(Console.ReadLine());
+
+            double gramsHoney = bees * flowers * 0.21;
+            double numP = Math.Floor(gramsHoney / 100);
+            double h = numP * 100;
+            double left = gramsHoney - h;
+
+            Console.WriteLine($"{numP} honeycombs filled.");
+            Console.WriteLine($"{left:f2} grams of honey left.");
+
+
+
+
+
+        }
+    }
+}
