31b3b082edcb65b2bb67102db309767d2065168b
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-11-14 13:02:41 +0200

    Add files via upload

---
+++ Exam Preparation/06. HoneyWinterReserves/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _06._HoneyWinterReserves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double honeyWinter = double.Parse(Console.ReadLine());
+            string inputOrName = Console.ReadLine();
+            double sumHoney = 0;
+
+            while (inputOrName != "Winter has come")
+            {
+
+                for (int i = 1; i <= 6; i++)
+                {
+                    
+                        double honeyS = double.Parse(Console.ReadLine());
+                        sumHoney += honeyS;
+
+                    
+                    
+                }
+
+
+                if (sumHoney < 0)
+                {
+                    Console.WriteLine($"{inputOrName} was banished for gluttony");
+
+                }
+                if (sumHoney >= honeyWinter)
+                {
+                    Console.WriteLine($"Well done! Honey surplus {sumHoney - honeyWinter:f2}.");
+
+                }
+                else if (sumHoney < honeyWinter)
+                {
+                    Console.WriteLine($"Hard Winter! Honey needed {honeyWinter - sumHoney:f2}.");
+
+
+                }
+
+            }
+        }
+    }
+}
+    
+
