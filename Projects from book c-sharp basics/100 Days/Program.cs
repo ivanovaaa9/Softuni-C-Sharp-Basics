3da4086957e8e39acaaf63695540e510ebfbaa01
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-11-15 17:46:48 +0200

    Add files via upload

---
+++ Projects from book c-sharp basics/100 Days/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _100_DAYS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myDate = Console.ReadLine();
+
+            DateTime answer = DateTime.ParseExact(myDate, "dd-MM-yyyy", null);
+
+            Console.WriteLine(answer.AddDays(999).ToString("dd-MM-yyyy"));
+
+        }
+    }
+}
