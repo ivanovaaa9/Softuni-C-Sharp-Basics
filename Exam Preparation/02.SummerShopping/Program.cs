d3bf25a5d6ef00bc70a377ae0e15889b32eaa956
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-11-15 18:01:09 +0200

    Add files via upload

---
+++ Exam Preparation/02.SummerShopping/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SummerShopping
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int budget = int.Parse(Console.ReadLine());
+            double priceTower = double.Parse(Console.ReadLine());
+            int discountP = int.Parse(Console.ReadLine());
+
+            double umbrela = (2.0 / 3) * priceTower;
+            double flipflops = 0.75 * umbrela;
+            double bagP = 1.0 / 3 * (priceTower + flipflops);
+
+            double total = priceTower + umbrela + flipflops + bagP;
+            total -= (discountP / 100.00) * total;
+
+            if (budget >= total)
+            {
+                Console.WriteLine($"Annie's sum is {total:f2} lv. She has {budget - total:f2} lv. left.");
+            }
+            else
+            {
+                Console.WriteLine($"Annie's sum is {total:f2} lv. She needs {total - budget:f2} lv. more.");
+            }
+        }
+    }
+}
