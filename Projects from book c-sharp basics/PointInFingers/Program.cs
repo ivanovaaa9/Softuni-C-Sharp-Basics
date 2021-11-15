11d8d5504f5e38a978865ac192de70b7a30fee2b
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-11-15 17:49:18 +0200

    Add files via upload

---
+++ Projects from book c-sharp basics/PointInFingers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Book_pointInfigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            bool outRec1 = (x < 0 || x > 3 * h) || (y < 0 || y > h);
+            bool outRec2 = (x < h || x > 2 * h) || (y < h || y > 4 * h);
+
+            bool inRec1 = (x > 0 && x < 3 * h) && (y > 0 && y < h);
+            bool inRec2 = (x > h && x < 2 * h) && (y > h && y < 4 * h);
+
+            bool commonBorder = (x > h && x < 2 * h) && y == h;
+
+            if (outRec1 && outRec2)
+            {
+                Console.WriteLine("outside");
+            }else if ( inRec1 || inRec2 || commonBorder)
+            {
+                Console.WriteLine("inside");
+            }
+            else
+            {
+                Console.WriteLine("border");
+            }
+        }
+    }
+}
