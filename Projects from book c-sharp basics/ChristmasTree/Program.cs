c5419973df26959ec533d8dfa55252d726b9c2ce
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-11-15 17:50:12 +0200

    Add files via upload

---
+++ Projects from book c-sharp basics/ChristmasTree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChristmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i <=n; i++)
+            {
+                var stars = new string('*', i);
+                var space = new string(' ',n- i);
+                Console.Write(space);
+                Console.Write(stars);
+                Console.Write("|");
+                Console.Write(stars);
+                Console.WriteLine(space);
+
+            }
+        }
+    }
+}
