170436fa590741fe72f6a3923c7211fb8a168f12
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-11-15 17:50:58 +0200

    Add files via upload

---
+++ Projects from book c-sharp basics/CubByN/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CubNbyN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <=n; i++)
+            {
+                Console.WriteLine("*");
+                for(int r = 1; r < n; r++)
+                {
+                    Console.WriteLine(" *");
+                }
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
