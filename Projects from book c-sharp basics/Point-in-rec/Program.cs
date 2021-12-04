a348d22772b7d6e564b04b54d310b49164cfe762
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-12-04 12:13:30 +0200

    Add files via upload

---
+++ Projects from book c-sharp basics/Point-in-rec/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Point_in_rec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
