031828809c42019b20200269ddb4aff06e3f874b
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-12-04 12:12:30 +0200

    Add files via upload

---
+++ Projects from book c-sharp basics/NbyNstars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NbyNstars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= num; i++)
+            {
+
+                Console.Write(new string('*', num));
+            }
+        }
+    }
+}
