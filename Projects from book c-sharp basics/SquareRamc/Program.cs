e8b9333ba47ad97b905833c0aaa14ebbb93671b0
Цветелина Иванова <50923185+ivanovaaa9@users.noreply.github.com>
2021-12-04 12:18:54 +0200

    Add files via upload

---
+++ Projects from book c-sharp basics/SquareRamc/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SquareRamc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("+");
+            for (int up = 0; up < n-2; up++)
+            {               
+                Console.Write(" -");                
+            }
+            Console.WriteLine(" +");
+
+            for(int i = 0; i < n-2; i++)
+            {
+                Console.Write("|");
+                for(int r = 0; r < n-2; r++)
+                {
+                    Console.Write(" -");
+                }
+                Console.WriteLine(" |");
+            }
+            Console.Write("+");
+            for (int up = 0; up < n - 2; up++)
+            {
+                Console.Write(" -");
+            }
+            Console.WriteLine(" +");
+
+        }
+    }
+}
